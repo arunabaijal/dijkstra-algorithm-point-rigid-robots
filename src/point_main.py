13157d81ef2fdf53db371da24af888d706eaa570
Nalin Das <nalindas9@gmail.com>
2020-03-01 21:51:52 -0500

    Add main.py scripts for point and rigid robots

---
+++ src/point_main.py
@@ -0,0 +1,9 @@
+#*********************************** Point Robot ********************************#
+
+#Taking start point and goal point from the user
+start_point = input('Please enter the start point in this format - (x,y): ')
+print('The start point you gave is:', start_point)
+print('')
+goal_point = input('Please enter the goal point in this format - (x,y): ')
+print('The goal point you gave is:', goal_point)
+
