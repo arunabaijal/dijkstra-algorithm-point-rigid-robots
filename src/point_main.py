606640c809ec89ace873bb0db99e2b8fb9bc6691
Aruna Baijal <aruna11baijal@gmail.com>
2020-03-04 20:09:47 -0500

    Changing from dictionary to queue

--- src/point_main.py
+++ src/point_main.py
@@ -1,13 +1,14 @@
 #*********************************** Point Robot ********************************#
 import matplotlib.pyplot as plt
 import numpy as np
+import queue
 
 class Node():
-    def __init__(self, start_point, parent, cost2come, index):
+    def __init__(self, start_point, parent, cost2come, ind):
         self.current = start_point
         self.parent = parent
         self.cost2come = cost2come
-        self.index = index
+        self.index = ind
 
     def moveDown(self):
         new_position = [self.current[0], self.current[1] - 1]
@@ -65,155 +66,126 @@ class Node():
         else:
             return new_position, 'up left'
 
-    def bfs(self, goal):
+    def dijkstra(self, goal):
         open('Nodes.txt', 'w').close()  # clearing files
-        # open('NodesInfo.txt', 'w').close()
         accepted = dict()
-        rejected = dict()
-        toBeVisited = dict()
-        toBeVisited[self.index] = self
+        visited = dict()
+        toBeVisited = queue.Queue()
+        toBeVisited.put(self)
         accepted[self.index] = self
         f = open("Nodes.txt", "a+")
-        # f2 = open("NodesInfo.txt", "a+")
-        # countMax = 1
-        while (len(toBeVisited) != 0):
-            # count = 0
-            ind = 0
-            for key,value in toBeVisited.items():
-                ind = key
-                break
-            visitingNode = toBeVisited.pop(ind)
+        while not toBeVisited.empty():
+            visitingNode = toBeVisited.get()
             node = visitingNode.current
-            # print(tuple(node))
-            # f2.write(str(visitingNode.index) + ' ' + str(
-            # 	0 if visitingNode.parent == None else visitingNode.parent.index) + '\n')
-            if visitingNode.index in rejected:  # check if node already visited
+            if visitingNode.index in visited:  # check if node already visited
                 continue
             else:
                 toWrite = str(node)
                 f.write(toWrite[1:len(toWrite) - 1] + '\n')
                 if np.array_equal(node, goal):  # check if goal found
                     f.close()
-                    # f2.close()
                     return visitingNode
                 # create all possible children
-                del accepted[visitingNode.index]
-                rejected[visitingNode.index] = visitingNode
                 if visitingNode.moveUp():
                     new, up = visitingNode.moveUp()
-                    # if not (tuple(new.reshape(1, 9)[0]) in rejected):
-                    # 	count += 1
-                    new_index  = index(new)
+                    new_index = index(new)
                     new_node = Node(new, visitingNode, visitingNode.cost2come + 1, new_index)
-                    if not (new_index in rejected):
+                    if new_index not in visited:
                         if new_index in accepted:
                             if accepted[new_index].cost2come > new_node.cost2come:
                                 accepted[new_index] = new_node
-                                toBeVisited[new_index] = new_node
+                                toBeVisited.put(new_node)
                         else:
                             accepted[new_index] = new_node
-                            toBeVisited[new_index] = new_node
+                            toBeVisited.put(new_node)
                 if visitingNode.moveDown():
                     new, down = visitingNode.moveDown()
-                    # if not (tuple(new.reshape(1, 9)[0]) in visited):
-                    # 	count += 1
                     new_index = index(new)
                     new_node = Node(new, visitingNode, visitingNode.cost2come + 1, new_index)
-                    if not (new_index in rejected):
+                    if new_index not in visited:
                         if new_index in accepted:
                             if accepted[new_index].cost2come > new_node.cost2come:
                                 accepted[new_index] = new_node
-                                toBeVisited[new_index] = new_node
+                                toBeVisited.put(new_node)
                         else:
                             accepted[new_index] = new_node
-                            toBeVisited[new_index] = new_node
+                            toBeVisited.put(new_node)
                 if visitingNode.moveRight():
                     new, right = visitingNode.moveRight()
-                    # if not (tuple(new.reshape(1, 9)[0]) in visited):
-                    # 	count += 1
                     new_index = index(new)
                     new_node = Node(new, visitingNode, visitingNode.cost2come + 1, new_index)
-                    if not (new_index in rejected):
+                    if new_index not in visited:
                         if new_index in accepted:
                             if accepted[new_index].cost2come > new_node.cost2come:
                                 accepted[new_index] = new_node
-                                toBeVisited[new_index] = new_node
+                                toBeVisited.put(new_node)
                         else:
                             accepted[new_index] = new_node
-                            toBeVisited[new_index] = new_node
+                            toBeVisited.put(new_node)
                 if visitingNode.moveLeft():
                     new, left = visitingNode.moveLeft()
-                    # if not (tuple(new.reshape(1, 9)[0]) in visited):
-                    # 	count += 1
                     new_index = index(new)
                     new_node = Node(new, visitingNode, visitingNode.cost2come + 1, new_index)
-                    if not (new_index in rejected):
+                    if new_index not in visited:
                         if new_index in accepted:
                             if accepted[new_index].cost2come > new_node.cost2come:
                                 accepted[new_index] = new_node
-                                toBeVisited[new_index] = new_node
+                                toBeVisited.put(new_node)
                         else:
                             accepted[new_index] = new_node
-                            toBeVisited[new_index] = new_node
+                            toBeVisited.put(new_node)
                 if visitingNode.moveUpRight():
                     new, upRight = visitingNode.moveUpRight()
-                    # if not (tuple(new.reshape(1, 9)[0]) in visited):
-                    # 	count += 1
                     new_index = index(new)
                     new_node = Node(new, visitingNode, visitingNode.cost2come + np.sqrt(2), new_index)
-                    if not (new_index in rejected):
+                    if new_index not in visited:
                         if new_index in accepted:
+                            print(accepted[new_index].cost2come, new_node.cost2come)
                             if accepted[new_index].cost2come > new_node.cost2come:
                                 accepted[new_index] = new_node
-                                toBeVisited[new_index] = new_node
+                                toBeVisited.put(new_node)
                         else:
                             accepted[new_index] = new_node
-                            toBeVisited[new_index] = new_node
+                            toBeVisited.put(new_node)
                 if visitingNode.moveDownRight():
                     new, downRight = visitingNode.moveDownRight()
-                    # if not (tuple(new.reshape(1, 9)[0]) in visited):
-                    # 	count += 1
                     new_index = index(new)
                     new_node = Node(new, visitingNode, visitingNode.cost2come + np.sqrt(2), new_index)
-                    if not (new_index in rejected):
+                    if new_index not in visited:
                         if new_index in accepted:
                             if accepted[new_index].cost2come > new_node.cost2come:
                                 accepted[new_index] = new_node
-                                toBeVisited[new_index] = new_node
+                                toBeVisited.put(new_node)
                         else:
                             accepted[new_index] = new_node
-                            toBeVisited[new_index] = new_node
+                            toBeVisited.put(new_node)
                 if visitingNode.moveUpLeft():
                     new, upLeft = visitingNode.moveUpLeft()
-                    # if not (tuple(new.reshape(1, 9)[0]) in visited):
-                    # 	count += 1
                     new_index = index(new)
                     new_node = Node(new, visitingNode, visitingNode.cost2come + np.sqrt(2), new_index)
-                    if not (new_index in rejected):
+                    if new_index not in visited:
                         if new_index in accepted:
                             if accepted[new_index].cost2come > new_node.cost2come:
                                 accepted[new_index] = new_node
-                                toBeVisited[new_index] = new_node
+                                toBeVisited.put(new_node)
                         else:
                             accepted[new_index] = new_node
-                            toBeVisited[new_index] = new_node
+                            toBeVisited.put(new_node)
                 if visitingNode.moveDownLeft():
                     new, downLeft = visitingNode.moveDownLeft()
-                    # if not (tuple(new.reshape(1, 9)[0]) in visited):
-                    # 	count += 1
                     new_index = index(new)
                     new_node = Node(new, visitingNode, visitingNode.cost2come + np.sqrt(2), new_index)
-                    if not (new_index in rejected):
+                    if new_index not in visited:
                         if new_index in accepted:
                             if accepted[new_index].cost2come > new_node.cost2come:
                                 accepted[new_index] = new_node
-                                toBeVisited[new_index] = new_node
+                                toBeVisited.put(new_node)
                         else:
                             accepted[new_index] = new_node
-                            toBeVisited[new_index] = new_node
-                # countMax = countMax + count
+                            toBeVisited.put(new_node)
+                del accepted[visitingNode.index]
+                visited[visitingNode.index] = visitingNode
         f.close()
-        # f2.close()
         return False
 
 def create_graph(xmax = 200, ymax = 100):
@@ -249,7 +221,6 @@ def generate_path(node, root):
         content = f.read()
         f.seek(0, 0)
         toWrite = str(node.current)
-        print(node.current)
         node = node.parent
         f.write(toWrite[1:len(toWrite) - 1] + '\n' + content)
         f.close()
@@ -257,7 +228,6 @@ def generate_path(node, root):
     f = open("nodePath.txt", "r+")
     content = f.read()
     f.seek(0, 0)
-    print(node.current)
     toWrite = str(node.current)
     f.write(toWrite[1:len(toWrite) - 1] + '\n' + content)
     f.close()
@@ -301,22 +271,22 @@ def main():
     plt.axis('scaled')
 
     start = Node(start_point, None, 0, index(start_point))
-    goal = start.bfs(goal_point)
+    goal = start.dijkstra(goal_point)
+    open('nodePath.txt', 'w').close()
     generate_path(goal, start_point)
 
     # Animating the explored nodes
+    file = open('Nodes.txt', 'r')
+    points = file.readlines()
+    for point in points:
+        pts = point.split(',')
+        plt.scatter(int(pts[0]), int(pts[1]), c='b')
+        # plt.pause(0.05)
     file = open('nodePath.txt', 'r')
     points = file.readlines()
     for point in points:
         pts = point.split(',')
-        # print(int(pts[0]), int(pts[1]))
         plt.scatter(int(pts[0]), int(pts[1]), c='g')
-        # plt.pause(0.05)
-    # for i in range(25):
-    # 	new  = up(start_point)
-    # 	start_point = new
-    # 	plt.scatter(start_point[0], start_point[1])
-    # 	plt.pause(0.25)
 
     plt.show()
 
